52be72185e8f34a1f2a49bebe86243fa64539264
akmnua <akmnua@users.noreply.github.com>
2019-01-17 22:09:31 -0500

    Update discovery.js

--- actions/discovery.js
+++ actions/discovery.js
@@ -66,7 +66,7 @@ function main(params) {
             bodySnippet: (body.length < 144 ? body : (body.substring(0,144) + '...')).replace(/<\/?[a-zA-Z]+>/g, ''),
             confidence: data.results[i].score,
             id: data.results[i].id,
-            session_token: data.session_token
+            session_token: data.session_token,
             sourceUrl: ("metadata" in data.results[i] ? data.results[i].metadata.source.url : ""),
             title: data.results[i].subtitle
           };
